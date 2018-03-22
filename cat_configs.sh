7919c31b758df16ce113e2720a5f16795a508f8d
ytelitenoob <ytelitenoob@gmail.com>
2018-03-22 18:32:24 +0000

    Turned a repeated text into a var, to shorten the lenght of the printf

--- cat_configs.sh
+++ cat_configs.sh
@@ -3,15 +3,17 @@ clear
 
 arg="$1"
 repo="$HOME/Projects/dotfiles_archpad/"
+text="The file has been copied to the repo"
 case $arg in
   -c| --catconfigs)
     cat $HOME/cat_configs.sh > $repo/cat_configs.sh
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   -h| --help)
     printf "\nDescription"
     printf "\nBash script that copies the selected type of config file to the dotfiles repo."
     printf "\n\nUsage:"
+    printf "\n\t -c, --catconfigs \n\t\tCopies the script to the repo"
     printf "\n\t -h, --help \n\t\tShows this menu."
     printf "\n\t -i, --i3wm \n\t\tCopies the i3 config file."
     printf "\n\t -n, --neovim \n\t\tCopies the neovim config file."
@@ -21,26 +23,25 @@ case $arg in
     ;;
   -i| --i3wm)
     cat $HOME/.config/i3/config > $repo/i3/config
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   -n| --nvim)
     cat $HOME/.config/nvim/init.vim > $repo/nvim/init.vim
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   -p| --polybar)
     cat $HOME/.config/polybar/config > $repo/polybar/config
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   -x| --xresources) 
     cat $HOME/.Xresources > $repo/Xresources
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   -z| --zshrc)
     cat $HOME/.zshrc > $repo/zsh/zshrc
-    printf "The file has been copied to the repo"
+    printf "$text"
     ;;
   *)
-    $print
     printf "\nUnknow operation $1" 
     printf "\nUsage: $0 -{h|i|n|p|x} "
   esac
