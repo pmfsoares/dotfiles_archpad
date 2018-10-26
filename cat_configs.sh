6f54fcabf02e34a88a8ede190dbb9f529c104375
ytelitenoob <ytelitenoob@gmail.com>
2018-10-27 00:03:48 +0100

    Added the '-a/-all' option to the script

--- cat_configs.sh
+++ cat_configs.sh
@@ -4,15 +4,17 @@ clear
 arg="$1"
 repo="$HOME/Projects/dotfiles_archpad/"
 text="The file has been copied to the repo"
+textall="The files has been copied to the repo"
 case $arg in
   -c| --catconfigs)
-    cat $HOME/cat_configs.sh > $repo/cat_configs.sh
+    cat $HOME/Projects/cat_configs.sh > $repo/cat_configs.sh
     printf "$text"
     ;;
   -h| --help)
     printf "\nDescription"
     printf "\nBash script that copies the selected type of config file to the dotfiles repo."
     printf "\n\nUsage:"
+    printf "\n\t -a, --all \n\t\tCopies every config file"
     printf "\n\t -c, --catconfigs \n\t\tCopies the script to the repo"
     printf "\n\t -h, --help \n\t\tShows this menu."
     printf "\n\t -i, --i3wm \n\t\tCopies the i3 config file."
@@ -20,6 +22,14 @@ case $arg in
     printf "\n\t -p, --polybar \n\t\tCopies the polybar config file."
     printf "\n\t -x, --xresources \n\t\tCopies the Xresources file."
     printf "\n\t -z, --zshrc \n\t\tCopies the zshrc file."
+    ;;
+  -a| --all)
+    cat $HOME/.config/i3/config > $repo/i3/config
+    cat $HOME/.config/nvim/init.vim > $repo/nvim/init.vim
+    cat $HOME/.config/polybar/config > $repo/polybar/config
+    cat $HOME/.Xresources > $repo/Xresources
+    cat $HOME/.zshrc > $repo/zsh/zshrc
+    printf "$textall"
     ;;
   -i| --i3wm)
     cat $HOME/.config/i3/config > $repo/i3/config
